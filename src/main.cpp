d3b24a1ddb04d6e86dddec2964fc905a81fcde75
Axlit Eels <axlit999@gmail.com>
2026-07-07 20:41:36 +0300

    main() sketch

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,33 @@
+#include "ecs/world.hpp"
+#include "systems.hpp"
+#include "components.hpp"
+
+using namespace cmps;
+
 int main() {
+    ecs::World world<TransformComponent, HealthComponent>();
+
+
+    ecs::Entity player = world.spawn();
+    world.emplace_component<TransformComponent>(player, 0, 0, 0);
+    world.emplace_component<HealthComponent>(player, 10);
+
+    ecs::Entity enemy = world.spawn();
+    world.emplace_component<TransformComponent>(enemy, 1, 2, 5);
+    world.emplace_component<HealthComponent>(enemy, 30);
+
+    ecs::Entity brick = world.spawn();
+    world.emplace_component<TransformComponent>(brick, -2, -2, -2);
+
+
+    systems::MovementSystem movement(-5, 0, 3);
+    systems::DrawingSystem drawing;
+
+    world.add_systems(movement, drawing);
+
+    for (int i = 0; i < 3; i++) {
+        world.update();
+    }
 
     return 0;
 }
